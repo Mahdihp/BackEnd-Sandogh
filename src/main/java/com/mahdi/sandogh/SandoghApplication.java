58f4ebb7a689a4212f876bc6957c18c8b5882590
mahdi <mahdihp.devsc@gmail.com>
2019-03-08 09:47:38 +0330

    Git Init.

---
+++ src/main/java/com/mahdi/sandogh/SandoghApplication.java
@@ -0,0 +1,13 @@
+package com.mahdi.sandogh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandoghApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SandoghApplication.class, args);
+    }
+
+}
